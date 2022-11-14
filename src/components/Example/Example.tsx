c9e693c2c9dcb3831f6e177c7255f0ba803992f6
Irving Dinh <irving.dinh@gmail.com>
2022-11-14 21:08:22 +0700

    NO-JIRA: Integrated with React

---
+++ src/components/Example/Example.tsx
@@ -0,0 +1,9 @@
+import styles from './Example.module.css';
+
+export const Example = () => {
+    return (
+        <section className={styles.section}>
+            <h1 className={styles.heading}>Hello, world!</h1>
+        </section>
+    )
+}
